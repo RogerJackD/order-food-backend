7a4bdce5698fc7e68d36a4f45503c71437ed6560
RogerJackD <1511638@senati.pe>
2025-06-22 23:42:54 -0500

    se agrego funcion de query dni

--- src/pedido-trabajador-cliente/pedido-trabajador-cliente.module.ts
+++ src/pedido-trabajador-cliente/pedido-trabajador-cliente.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PedidoTrabajadorClienteService } from './pedido-trabajador-cliente.service';
 import { PedidoTrabajadorClienteController } from './pedido-trabajador-cliente.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TrabajadorCliente } from 'src/trabajador-cliente/entities/trabajador-cliente.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TrabajadorCliente])],
   controllers: [PedidoTrabajadorClienteController],
   providers: [PedidoTrabajadorClienteService],
+
 })
 export class PedidoTrabajadorClienteModule {}
--- src/pedido-trabajador-cliente/pedido-trabajador-cliente.service.ts
+++ src/pedido-trabajador-cliente/pedido-trabajador-cliente.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePedidoTrabajadorClienteDto } from './dto/create-pedido-trabajador-cliente.dto';
 import { UpdatePedidoTrabajadorClienteDto } from './dto/update-pedido-trabajador-cliente.dto';
-import { ValidateDniDto } from './dto/validate-dni.dto';
+import { TrabajadorCliente } from 'src/trabajador-cliente/entities/trabajador-cliente.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PedidoTrabajadorClienteService {
+  constructor(
+    @InjectRepository(TrabajadorCliente)
+    private readonly trabajadorClienteRepository: Repository<TrabajadorCliente>,
+  ) {}
+
   create(createPedidoTrabajadorClienteDto: CreatePedidoTrabajadorClienteDto) {
     return 'This action adds a new pedidoTrabajadorCliente';
   }
@@ -13,11 +20,33 @@ export class PedidoTrabajadorClienteService {
     return `This action returns all pedidoTrabajadorCliente`;
   }
 
-  findOne(dni: string) {
-    return `This action returns a #${dni} pedidoTrabajadorCliente`;
+  async findOne(dni: string) {
+    const trabajador = await this.trabajadorClienteRepository
+      .createQueryBuilder('tc')
+      .innerJoin('tc.trabajador', 'p')
+      .select([
+        'tc.idTrabajador',
+        'p.idTipoDocumentoIdentidad',
+        'p.numeroDocumentoIdentidad',
+        'p.apellidoCompleto',
+        'p.nombreCompleto',
+        'p.razonSocial',
+        'p.direccion',
+      ])
+      .where('p.numeroDocumentoIdentidad = :dni', { dni })
+      .andWhere('p.idTipoDocumentoIdentidad = :tipoDoc', { tipoDoc: 2 })
+      .andWhere('p.indicadorEstado = :estado', { estado: 'A' })
+      .andWhere('tc.indicadorEstado = :estado', { estado: 'A' })
+      .getRawOne();
+
+      
+      return trabajador;
   }
 
-  update(id: number, updatePedidoTrabajadorClienteDto: UpdatePedidoTrabajadorClienteDto) {
+  update(
+    id: number,
+    updatePedidoTrabajadorClienteDto: UpdatePedidoTrabajadorClienteDto,
+  ) {
     return `This action updates a #${id} pedidoTrabajadorCliente`;
   }
 
