80c5c7e1a1340e0e3b5c455b0a6cc84790093fb3
RogerJackD <1511638@senati.pe>
2025-06-23 14:24:50 -0500

    refactor: delivery endpoint

--- src/pedido-trabajador-cliente/pedido-trabajador-cliente.service.ts
+++ src/pedido-trabajador-cliente/pedido-trabajador-cliente.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePedidoTrabajadorClienteDto } from './dto/create-pedido-trabajador-cliente.dto';
 import { UpdatePedidoTrabajadorClienteDto } from './dto/update-pedido-trabajador-cliente.dto';
 import { TrabajadorCliente } from 'src/trabajador-cliente/entities/trabajador-cliente.entity';
@@ -12,35 +12,36 @@ export class PedidoTrabajadorClienteService {
     private readonly trabajadorClienteRepository: Repository<TrabajadorCliente>,
   ) {}
 
+  
+  async findOne (dni: string) {
+    const trabajador = await this.trabajadorClienteRepository
+    .createQueryBuilder('tc')
+    .innerJoin('tc.trabajador', 'p')
+    .select([
+      'tc.idTrabajador',
+      'p.idTipoDocumentoIdentidad',
+      'p.numeroDocumentoIdentidad',
+      'p.apellidoCompleto',
+      'p.nombreCompleto',
+      'p.razonSocial',
+      'p.direccion',
+    ])
+    .where('p.numeroDocumentoIdentidad = :dni', { dni })
+    .andWhere('p.idTipoDocumentoIdentidad = :tipoDoc', { tipoDoc: 2 })
+    .andWhere('p.indicadorEstado = :estado', { estado: 'A' })
+    .andWhere('tc.indicadorEstado = :estado', { estado: 'A' })
+    .getRawOne();
+    
+    if(!trabajador)
+      throw new BadRequestException("trabajador no econtrado")
+    return {nombres : trabajador.p_RazonSocial};
+  }
   create(createPedidoTrabajadorClienteDto: CreatePedidoTrabajadorClienteDto) {
     return 'This action adds a new pedidoTrabajadorCliente';
   }
 
   findAll() {
     return `This action returns all pedidoTrabajadorCliente`;
-  }
-
-  async findOne(dni: string) {
-    const trabajador = await this.trabajadorClienteRepository
-      .createQueryBuilder('tc')
-      .innerJoin('tc.trabajador', 'p')
-      .select([
-        'tc.idTrabajador',
-        'p.idTipoDocumentoIdentidad',
-        'p.numeroDocumentoIdentidad',
-        'p.apellidoCompleto',
-        'p.nombreCompleto',
-        'p.razonSocial',
-        'p.direccion',
-      ])
-      .where('p.numeroDocumentoIdentidad = :dni', { dni })
-      .andWhere('p.idTipoDocumentoIdentidad = :tipoDoc', { tipoDoc: 2 })
-      .andWhere('p.indicadorEstado = :estado', { estado: 'A' })
-      .andWhere('tc.indicadorEstado = :estado', { estado: 'A' })
-      .getRawOne();
-
-      
-      return trabajador;
   }
 
   update(
