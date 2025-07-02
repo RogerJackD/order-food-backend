b41c2b646799d3fe2423a6a364f8c80397df0029
RogerJackD <1511638@senati.pe>
2025-07-02 11:35:13 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS habilitado para todos los orígenes
+  app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
